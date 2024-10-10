82994d62d702d3d4643ef17d800a448e4ad56c59
antony.novaes <antonynovaescruz@gmail.com>
2024-10-10 13:59:22 -0300

    lição funcionando

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const calcular = document.querySelector('button')
+function multiplicar() {
+    const numero1 = Number(document.querySelector("#numero1").value)
+    const numero2 = Number(document.querySelector("#numero2").value)
+    const numero3 = Number(document.querySelector("#numero3").value)
+
+    const paragrafo = document.querySelector(".resultado")
+
+    paragrafo.innerHTML = `Resultado: ${numero1 * numero2 * numero3}`
+}
+
+const button = document.querySelector("button")
+button.addEventListener('click', multiplicar)
